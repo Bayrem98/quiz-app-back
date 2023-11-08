2f5b548dd0e9b96ff26f3d3d8a6be55e6eb2f551
bayrem98 <bayremsayeh98@gmail.com>
2023-11-08 13:40:40 +0100

    remove cors code in maints file

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: '*',
-    },
-  });
+  const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
